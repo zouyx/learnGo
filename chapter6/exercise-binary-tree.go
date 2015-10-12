c728522127d1f48076f7bec6df0dbe00e8485f32
joezou <yixian.zou@gmail.com>
2015-10-12 23:04:36 +0800

    update binary tree exercise

--- chapter6/exercise-binary-tree.go
+++ chapter6/exercise-binary-tree.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"math/rand"
 )
 
+type Tree struct{
+	Left *Tree
+	Value int
+	Right *Tree
+}
+
+func insert (t *Tree, i int) *Tree{
+	if t== nil{
+		return &Tree{nil, i, nil}
+	}
+
+	if(i< t.Value){
+		t.Left= insert(t.Left, i)
+		return t
+	}else{
+		t.Right= insert(t.Right, i)
+		return t
+	}
+}
+
 func main() {
-	c := make(chan int)
-	quit := make(chan int)
-	
-	go func(){
-		for i:=0; i<10; i++ {
-			fmt.Println(<-c)
-		}
-		quit <-0
-	}()
-	
-	fibonacci(c, quit)
+	r:= rand.New(rand.NewSource(time.Now().UnixNano()))
+	t:= insert(nil, r.Intn(50))
+	insert(t, r.Intn(50))
+
+	fmt.Println(t.Value,t.Right.Value)
 }
