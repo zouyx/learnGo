ca69c67fdcee659dd4d4716e605b12c28ef5578b
joezou <yixian.zou@gmail.com>
2015-10-13 22:34:15 +0800

    modify logical of binary tree compare.

--- chapter6/exercise-binary-tree.go
+++ chapter6/exercise-binary-tree.go
@@ -2,34 +2,85 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
-type Tree struct{
-	Left *Tree
+type Tree struct {
+	Left  *Tree
 	Value int
 	Right *Tree
 }
 
-func insert (t *Tree, i int) *Tree{
-	if t== nil{
+func Insert(t *Tree, i int) *Tree {
+	if t == nil {
 		return &Tree{nil, i, nil}
 	}
 
-	if(i< t.Value){
-		t.Left= insert(t.Left, i)
+	if i < t.Value {
+		t.Left = Insert(t.Left, i)
 		return t
-	}else{
-		t.Right= insert(t.Right, i)
+	} else {
+		t.Right = Insert(t.Right, i)
 		return t
 	}
 }
 
-func main() {
-	r:= rand.New(rand.NewSource(time.Now().UnixNano()))
-	t:= insert(nil, r.Intn(50))
-	insert(t, r.Intn(50))
+func CreateTree() *Tree {
+	var t *Tree
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0 ; i<10 ; i++ {
+		t = Insert(t, r.Intn(50))
+	}
+	return t
+}
 
-	fmt.Println(t.Value,t.Right.Value)
+func PrintTree(t *Tree, c chan int) {
+	if(t==nil){
+		return
+	}
+
+	PrintTree(t.Left,c)	
+	c <- t.Value
+	PrintTree(t.Right,c)	
+} 
+
+func Print(t *Tree, c chan int) {
+	PrintTree(t, c)
+
+	close(c)
+}
+
+func Compare (t1 *Tree,t2 *Tree) bool{
+	c1 := make(chan int)
+	c2 := make(chan int)
+	
+	go Print(t1, c1)
+	go Print(t2, c2)
+
+	var flag bool=true
+	
+	for {
+		i, iok := <- c1
+		j, jok := <- c2
+		if(!iok||!jok){
+			break
+		}
+
+		fmt.Println(i, j)
+		if(i != j){
+			flag = false
+			break
+		}
+	}
+	
+	return flag
+}
+
+func main() {
+	t1 := CreateTree()
+	t2 := CreateTree()
+	
+	flag :=	Compare(t1, t2)
+	fmt.Println("value:",flag)
 }
