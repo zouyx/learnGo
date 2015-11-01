1980e2d53e9a77feb2a8eaa22aa428969600e03b
joezou <yixian.zou@gmail.com>
2015-11-01 16:03:53 +0800

    add items.go for chapter7

---
+++ wayToGo/chapter7/items.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	items := [...]int{10, 20, 30, 40, 50}
+	for _, item := range items {
+   		 item *= 2
+		fmt.Println(item)
+	}
+	fmt.Println(items)
+}
+
